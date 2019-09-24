a26fa1fae8435479fc53f8f4184fd76eede1d405
Rohan Patil <rohan.patil1@tcs.com>
2019-09-24 14:21:50 +0530

    ngIf:Header Tab Selection & recipe list item added

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipe } from '../../recipe.model';
 
 
 @Component({
@@ -8,8 +9,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeItemComponent implements OnInit {
 
+  @Input() recipe: Recipe;
   constructor() { }
-
   ngOnInit() {
   }
 
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,8 +9,8 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('Recipe1', 'This is a Test Recipe 1 ', 'imgPath1'),
-    new Recipe('Recipe2', 'This is a Test Recipe 2', 'imgPath2')
+    new Recipe('EggPlant Parmesan', 'This is a Recipe for EggPlant Parmesan ', 'https://www.tablefortwoblog.com/wp-content/uploads/2018/05/eggplant-parmesan-recipe-photos-tablefortwoblog-1.jpg'),
+    new Recipe('Crescent', 'Recipe for making delicious Crescent', 'https://images-gmi-pmc.edge-generalmills.com/23bcd559-1e27-4c54-90a7-812690764c7f.jpg')
   ];
   constructor() { }
 
