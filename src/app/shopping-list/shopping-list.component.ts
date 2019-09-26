14d2a3e3bdf715cd473fe8c225261ee930257baa
Rohan Patil <rohan.patil1@tcs.com>
2019-09-26 18:57:11 +0530

    Edit Shopping List

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -17,4 +17,11 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
+  
+
+
 }
