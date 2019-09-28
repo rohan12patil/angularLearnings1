53f795fd3a024e4cc4d05ad14d231c668e3754d0
Rohan Patil <rohan.patil1@tcs.com>
2019-09-28 12:20:40 +0530

    Dropdown Directive Added

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -8,8 +8,8 @@ import { Ingredient } from 'src/app/shared/ingredient.model';
 })
 export class ShoppingEditComponent implements OnInit {
 
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @ViewChild('nameInput',{static: false}) nameInputRef: ElementRef;
+  @ViewChild('amountInput',{static: false}) amountInputRef: ElementRef;
   @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
